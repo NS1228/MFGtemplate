16397725d1e4cb0b3a1f60323ac4ae541e5fefcc
Unknown <ns1228yt@gmail.com>
2019-05-22 21:32:13 +0100

    wsfkaskfksdfl;sdf;

---
+++ Assets/Car_Controls.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Controls : MonoBehaviour
+{
+
+    public float acceleration;
+    public float rotationRate;
+
+    public float turnRotationAngle;
+    public float turnRotationSeekSpeed;
+
+    private float rotationVelocity;
+    private float groundAngleVelocity;
+
+    Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start ()
+    {
+       _rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+   void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.up*-1, 3f))
+        {
+            _rb.drag = 1;
+
+            Vector3 forwardForce = transform.forward * acceleration * Input.GetAxis("Vertical");
+
+            forwardForce = forwardForce * Time.deltaTime * _rb.mass;
+
+            _rb.AddForce(forwardForce);
+        }
+        else
+        {
+            _rb.drag = 0;
+        }
+        Vector3 turnTorque = Vector3.up * rotationRate * Input.GetAxis("Horizontal");
+
+        turnTorque = turnTorque * Time.deltaTime * _rb.mass;
+        _rb.AddTorque(turnTorque);
+
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z = Mathf.SmoothDampAngle(newRotation.z, Input.GetAxis("Horizontal") * -turnRotationAngle, ref rotationVelocity, turnRotationSeekSpeed);
+        transform.eulerAngles = newRotation;
+
+
+        }
+    }
+
---
+++ Assets/Thruster_script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster_script : MonoBehaviour
+{
+    public float thrusterStrength;
+    public float thrusterDistance;
+    public Transform[] thrusters;
+
+    Rigidbody rb;
+
+    void start ()
+    {
+       rb = gameObject.GetComponent<Rigidbody>();
+    }
+        
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        foreach (Transform thruster in thrusters)
+        { 
+            Vector3 downwardForce;
+            float distancePercentage;
+
+            if(Physics.Raycast (thruster.position, thruster.up *-1, out hit, thrusterDistance))
+            {
+                distancePercentage = 1 - (hit.distance / thrusterDistance);
+
+                downwardForce = transform.up * thrusterStrength * distancePercentage;
+
+                downwardForce = downwardForce * Time.deltaTime * rb.mass;
+
+                rb.AddForceAtPosition(downwardForce, thruster.position);
+            }
+        }
+
+  }
+}

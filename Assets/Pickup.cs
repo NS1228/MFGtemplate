a97a842d3bb8ce7d9ab7dfa51a7c61ed18dc6529
Unknown <ns1228yt@gmail.com>
2019-06-03 18:27:58 +0100

    jdsfgdsjiofgjsdoifjsdiofj

---
+++ Assets/Pickup.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    float throwForce = 600;
+    Vector3 ObjectPos;
+    float distance;
+
+    public bool canHold = true;
+    public GameObject item;
+    public GameObject tempParent;
+    public bool isHolding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isHolding)
+        {
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            item.transform.SetParent(tempParent.transform); 
+
+
+            if(Input.GetMouseButton(1))
+            {
+
+            }
+            else
+            {
+                ObjectPos = item.transform.position;
+                item.transform.SetParent(null);
+                item.GetComponent<Rigidbody>().useGravity = true;
+                item.transform.position = ObjectPos;
+                     
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isHolding = true;
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().detectCollisions = true;
+    }
+
+    void OnMouseUp ()
+    {
+        isHolding = false;
+    }
+}
---
+++ Assets/Working scripts/Teleport_Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport_Test : MonoBehaviour
+{
+    
+   public Transform destination;
+    public GameObject player;
+
+
+   void Update ()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            StartCoroutine(Startspawn());
+           
+        }
+    }
+
+
+    IEnumerator Startspawn ()
+    {
+        yield return new WaitForSeconds(2);
+        player.transform.position = transform.position;
+        //yield return new WaitForSeconds(1);
+        this.gameObject.SetActive(false);
+        print("yes");
+    }
+    
+}

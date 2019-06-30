664bd8ade1cd18d7e2dd487341bb04fe53c847d6
Unknown <ns1228yt@gmail.com>
2019-06-30 13:25:37 +0100

    dsdasdsadggdg

---
+++ Assets/Enemy_Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+
+    public Transform Player;
+    public float MoveSpeed = 4;
+    public float MaxDist = 10;
+    public float MinDist = 5;
+
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //Here Call any function U want Like Shoot at here or something
+                rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX;
+
+            }
+
+        }
+    }
+}
---
+++ Assets/Player_takedamage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_takedamage : MonoBehaviour
+{
+    public float playerHealth;
+    public float axeDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Death();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Axe")
+        {
+            playerHealth -= axeDamage;
+            print("BITCH");
+        }
+    }
+
+    void Death ()
+    {
+        if(playerHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
